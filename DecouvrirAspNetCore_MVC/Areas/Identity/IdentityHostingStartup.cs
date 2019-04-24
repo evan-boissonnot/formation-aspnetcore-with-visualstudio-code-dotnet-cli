d05ee02476a57c7019500c618232a79b2167395b
evan-boissonnot <commercial@boissonnot.Fr>
2019-04-24 23:25:47 +0200

    preparing is ok

---
+++ DecouvrirAspNetCore_MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using DecouvrirAspNetCore_MVC.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DecouvrirAspNetCore_MVC.Areas.Identity.IdentityHostingStartup))]
+namespace DecouvrirAspNetCore_MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DecouvrirAspNetCore_MVCIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DecouvrirAspNetCore_MVCIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<DecouvrirAspNetCore_MVCIdentityDbContext>();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # formation-aspnetcore-with-visualstudio-code-dotnet-cli
 All asp.net core projet with dotnet cli
+
+
+# Commands used to create and manage projects in this solution
+
+## 00. Add global json
+dotnet new globaljson 
+
+open global.json, and change to 2.2.x if you don't want 3.x preview
+
+## 0. Create solution
+dotnet new sln
+
+## 1. Create project web
+dotnet new mvc --auth Individual --name DecouvrirAspNetCore_MVC
+
+## 2. Add project to solution
+dotnet sln add DecouvrirAspNetCore_MVC
+
+## 3. Display all pages from Identity Back office
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design 
+
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer 
+
+dotnet tool install --global dotnet-aspnet-codegenerator --version 2.2.3
+
+dotnet aspnet-codegenerator identity
+
+## 4. Add new controller
+
